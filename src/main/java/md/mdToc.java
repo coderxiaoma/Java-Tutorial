81f9c1fa411585e5c190cb0f76e64282afbd2e88
pony <mwsir@qq.com>
2025-03-26 01:26:37 +0800

    add comment

--- src/main/java/md/mdToc.java
+++ src/main/java/md/mdToc.java
@@ -4,6 +4,7 @@ import com.github.houbb.markdown.toc.core.impl.AtxMarkdownToc;
 
 public class mdToc {
     public static void main(String[] args) {
+        // 添加注释
         String path = "D:\\javaTutorial\\docs\\distrubuted\\practice\\";
         AtxMarkdownToc.newInstance().genTocDir(path);
     }
